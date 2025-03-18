a7b96bf787b1fced98386ac2a5f7906d08144f86
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2025-03-19 05:22:58 +0900

    🎨 Implemented LoadConfig function which is reading from .env file

---
+++ backend-go/cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nakachan-ing/fukuworks/backend-go/config"
+)
+
+func main() {
+	config.LoadConfig()
+	fmt.Println("Running in:", config.Env)
+	fmt.Println("Database Path:", config.DatabasePath)
+	fmt.Println("API URL:", config.ApiUrl)
+}
